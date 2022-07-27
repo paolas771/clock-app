52fa1a8798ce200725d613562f5ec9e42b0fffbd
mod771 <70249026+mod771@users.noreply.github.com>
2022-07-27 18:14:53 -0500

    add timer to update clock on screen

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Quote from './Components/Quote'
 import Time from './Components/Time'
 export default function App() {
   const [time, setTime] = React.useState('http://worldtimeapi.org/api/ip')
+  //timer to update the clock every minute
+  const [timer, setTimer] = useState(0)
+  setTimeout(() => {
+    setTimer(timer + 1)
+  }, 60000)
 
-    //move to app.js and pass props
-    React.useEffect(() => {
-        fetch('http://worldtimeapi.org/api/ip')
-        .then(res => res.json())
-        .then(info => setTime(info))
-    }, [])
+  React.useEffect(() => {
+    fetch('http://worldtimeapi.org/api/ip')
+    .then(res => res.json())
+    .then(info => setTime(info))
+  }, [timer])
 
+  
   return (
     <div className="App">
      <Quote />
      <Time 
-      time={time.datetime}/>
+      time={time.datetime}
+    />
     </div>
   );
 }
